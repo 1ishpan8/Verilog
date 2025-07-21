6acc16f7831987da83bcfb61d9e76a59f32662ea
1ishpan8 <ishaanpanda725@gmail.com>
2025-07-21 23:04:02 +0530

    Add files via upload
    Parallel adder

---
+++ paralleladder.v
@@ -0,0 +1,8 @@
+module parallel_adder (sum, cout, in1, in2, cin);
+       input [7:0] in1, in2;
+       input cin;
+       output [7:0] sum;
+       output cout;
+
+       assign #20 {cout,sum} = in1 + in2 + cin; // concatenation in case there is a carry in
+endmodule
